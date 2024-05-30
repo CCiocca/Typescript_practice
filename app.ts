82755c02eb59a82804ecd010bd67794dbba6918d
CCiocca <chiara.cioccarelli1@gmail.com>
2024-05-30 15:22:09 +0200

    object, array, tupla

--- app.ts
+++ app.ts
@@ -1,4 +1,33 @@
+//lesson 03
 function somma(a: number, b: number){
-    return a+b;
+    return a + b;
 }
 console.log(somma(5,2));
+
+// lesson 04: number, string, booleans
+let num1: number;
+let string1: string;
+let bool1: boolean;
+
+// lesson 05: objects, array, tuple
+let person: {
+    name: string,
+    surname: string,
+    age: number,
+    address: {
+        street: string;
+        number: number;
+        city: string;
+    }
+}
+
+person = {
+    name: 'John',
+    surname: 'Smith',
+    age: 22,
+    address: {
+        street: "via Rossi",
+        number: 45,
+        city: "Rome"
+    }
+}
